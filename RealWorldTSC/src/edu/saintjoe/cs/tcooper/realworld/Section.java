9868343883c1e6f3fb0feedf79c3ca78740b2403
tcooper <tcooper@localhost>
2016-04-25 17:25:15 -0400

    Created methods in Section

--- RealWorldTSC/src/edu/saintjoe/cs/tcooper/realworld/Section.java
+++ RealWorldTSC/src/edu/saintjoe/cs/tcooper/realworld/Section.java
@@ -6,5 +6,25 @@ public class Section {
 	String title;
 	Student[] roster;
 /*----------------------------------------------------------------------------*/
+	public void setRoom(String newRoom){
+		room = newRoom;
+	}
+	public String getRoom(){
+		return room;
+	}
+	
+	public void setTime(String newTime){
+		time = newTime;
+	}
+	public String getTime(){
+		return time;
+	}
+	
+	public void setTitle(String newTitle){
+		title = newTitle;
+	}
+	public String newTitle(){
+		return title;
+	}
 	
 }
